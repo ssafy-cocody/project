81a05057a3038a0bafdace0b1bb14b7607c5dcda
SSAFY <jmg97@kakao.com>
2024-03-07 17:55:10 +0900

    init

---
+++ backend/src/main/java/com/cocodi/CocodiApplication.java
@@ -0,0 +1,13 @@
+package com.cocodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocodiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocodiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/cocodi/CocodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocodiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
