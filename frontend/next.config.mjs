dcb14bee7af306c1cd19b16df21f8e792ba3b889
박진아 <dndbekfrl1@gmail.com>
2024-03-14 15:08:28 +0900

    [FE] [S10P22A307-158] eslint 규칙 수정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
-// const TerserPlugin = require('terser-webpack-plugin');
+
 import TerserPlugin from 'terser-webpack-plugin';
 const nextConfig = {
   webpack: (config) => {
