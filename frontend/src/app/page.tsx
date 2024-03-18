3af412580df08c1d55197c3bf9b7795a16e59a8f
박지영 <jiyounge628@gmail.com>
2024-03-18 15:51:20 +0900

    [S10P22A307-160] feat: 내코디 미리보기 레이아웃 구현

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,31 @@
 'use client';
 
 import Image from 'next/image';
-import Nav from '@/components/nav';
-import RecommendViewer from '@/containers/home/RecommendViewer';
-import RecommendTab from '@/containers/home/RecommendTab';
-import RecommendItems from '@/containers/home/RecommendItems';
-import styles from '@/containers/home/Home.module.scss';
 
-export default function Home() {
+import Nav from '@/components/nav';
+import styles from '@/containers/home/Home.module.scss';
+import MyCodyPreview from '@/containers/home/MyCodyPreview';
+import RecommendItems from '@/containers/home/RecommendItems';
+import RecommendTab from '@/containers/home/RecommendTab';
+import RecommendViewer from '@/containers/home/RecommendViewer';
+
+const Home = () => {
   return (
-    <>
-      <main>
-        <header className={styles['home-header']}>
-          <Image src="/images/logo.png" alt="Co.Cody 로고" width={130} height={40} />
-        </header>
-        <div className={styles['home-content']}>
-          <RecommendViewer />
-          <RecommendTab />
-          <RecommendItems />
-        </div>
-        <div className={styles['home-navigation']}>
-          <Nav></Nav>
-        </div>
-      </main>
-    </>
+    <main>
+      <header className={styles['home-header']}>
+        <Image src="/images/logo.png" alt="Co.Cody 로고" width={130} height={40} />
+      </header>
+      <div className={styles['home-content']}>
+        <RecommendViewer />
+        <RecommendTab />
+        <RecommendItems />
+        <MyCodyPreview />
+      </div>
+      <div className={styles['home-navigation']}>
+        <Nav />
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
