064867b26465576783286df3cd1d54c68dce4613
박지영 <jiyounge628@gmail.com>
2024-03-18 15:14:39 +0900

    [S10P22A307-160] refactor: lint 반영 수정 사항

--- frontend/src/containers/home/RecommendTab/index.tsx
+++ frontend/src/containers/home/RecommendTab/index.tsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { ITemperatures } from './type';
+import { useEffect, useState } from 'react';
+
 import styles from '@/containers/home/RecommendTab/Tab.module.scss';
+
 import { ReloadIcon } from '../../../../public/svgs';
+import { ITemperatures } from './type';
 
 const RecommendTab = () => {
   // weather: 0 맑음 3 구름많음 4 흐림
-  const [temperatures, setTemperatures] = useState<ITemperatures>({
+  const [temperatures] = useState<ITemperatures>({
     lowestTemperature: 4,
     hightestTemperature: 15,
     weather: 3,
   });
   const [weatherUrl, setWeather] = useState<string>('/images/weather/sunny.png');
-  const [nickname, setNickname] = useState<string>('햄밤식');
-  const [description, setDescription] = useState<string>('오늘은 조금 추워요.');
-  const [recommendCodies, setCodies] = useState<string[]>([
+  const [nickname] = useState<string>('햄밤식');
+  const [description] = useState<string>('오늘은 조금 추워요.');
+  const [recommendCodies] = useState<string[]>([
     '/images/test1.jpg',
     '/images/test2.jpg',
     '/images/test3.jpg',
     '/images/test4.jpg',
     '/images/test5.jpg',
   ]);
-  const [ootdIndex, setOOTD] = useState<Number>(3);
-  const [viewerIndex, setViewer] = useState<Number>(0);
+  const [ootdIndex] = useState<Number>(3);
+  const [viewerIndex] = useState<Number>(0);
 
   useEffect(() => {
     switch (temperatures.weather) {
@@ -35,26 +37,29 @@ const RecommendTab = () => {
       case 4:
         setWeather('/images/weather/overcast.png');
         break;
+      default:
+        setWeather('/images/weather/sunny.png');
+        break;
     }
   }, []);
 
   return (
     <div className={styles['recommendbox-container']}>
       <div className={styles['tab-container']}>
-        <div className={`${styles['tab']} ${styles['selected-tab']}`}>오늘</div>
-        <div className={`${styles['tab']}`}>내일</div>
-        <div className={`${styles['tab']} ${styles['final-tab']}`}>모레</div>
+        <div className={`${styles.tab} ${styles['selected-tab']}`}>오늘</div>
+        <div className={`${styles.tab}`}>내일</div>
+        <div className={`${styles.tab} ${styles['final-tab']}`}>모레</div>
       </div>
       <div className={styles['context-container']}>
-        <button className={styles['reload-button']}>
+        <button className={styles['reload-button']} type="button">
           <ReloadIcon />
         </button>
         <div className={styles['text-area']}>
           <div className={styles['temperature-container']}>
-            <div className={styles['temperature']}>
+            <div className={styles.temperature}>
               {temperatures.lowestTemperature.toString()}° / {temperatures.hightestTemperature.toString()}°
             </div>
-            <Image src={weatherUrl} alt={'날씨'} width={20} height={20} />
+            <Image src={weatherUrl} alt="날씨" width={20} height={20} />
           </div>
           <div className={styles['greeting-text']}>반가워요, {nickname}님</div>
           <div className={styles['weather-description']}>{description}</div>
@@ -63,12 +68,13 @@ const RecommendTab = () => {
           {recommendCodies.map((cody, index) => {
             return (
               <div
-                className={`${styles['cody-container']} ${ootdIndex === index ? styles['ootd'] : ''} ${viewerIndex === index ? styles['view-cody-container'] : ''}`}
+                key={cody}
+                className={`${styles['cody-container']} ${ootdIndex === index ? styles.ootd : ''} ${viewerIndex === index ? styles['view-cody-container'] : ''}`}
               >
                 <div
                   className={`${viewerIndex === index ? styles['view-image-container'] : styles['image-container']}`}
                 >
-                  <Image className={styles['cody-image']} src={cody} fill alt={'추천 코디'}></Image>
+                  <Image src={cody} fill alt="추천 코디" />
                 </div>
               </div>
             );
