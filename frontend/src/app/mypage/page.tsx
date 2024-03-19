c14011974fe26d82cde194047e0d67e0c3a1cee6
박진아 <dndbekfrl1@gmail.com>
2024-03-19 17:47:04 +0900

    [FE] [S10P22A307-173] 마이페이지 퍼블리싱

--- frontend/src/app/mypage/page.tsx
+++ frontend/src/app/mypage/page.tsx
@@ -1,5 +1,17 @@
+import Background from '@/components/background';
+import Header from '@/components/Header';
+import Nav from '@/components/nav';
+import Menu from '@/containers/mypage/Menu';
+
 const Page = () => {
-  return <>마이페이지</>;
+  return (
+    <>
+      <Header title="마이페이지" />
+      <Menu />
+      <Background $backgroundColor="skyBlue" />
+      <Nav />
+    </>
+  );
 };
 
 export default Page;
--- frontend/src/app/mypage/profile/page.tsx
+++ frontend/src/app/mypage/profile/page.tsx
@@ -1,4 +1,17 @@
+import Background from '@/components/background';
+import Header from '@/components/Header';
+import ProfileCard from '@/containers/mypage/profile/ProfileCard';
+import styles from '@/containers/mypage/profile/ProfileLayout.module.scss';
+
 const Page = () => {
-  return <>프로필 수정</>;
+  return (
+    <>
+      <Header previousLink="/mypage" title="내 정보 수정" RightComponent="저장" />
+      <div className={styles.container}>
+        <ProfileCard />
+      </div>
+      <Background $backgroundColor="skyBlue" />
+    </>
+  );
 };
 export default Page;
