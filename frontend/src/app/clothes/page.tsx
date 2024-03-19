918c5936bdf958bd7c3f61a570b39df958d10ef2
박진아 <dndbekfrl1@gmail.com>
2024-03-19 15:44:51 +0900

    [FE] [S10P22A307-156] 빌드 에러 수정

--- frontend/src/app/clothes/page.tsx
+++ frontend/src/app/clothes/page.tsx
@@ -1,17 +1,7 @@
-import AdditionalForm from '@/containers/clothes/AdditionalForm';
-import SearchWithCode from '@/containers/clothes/SearchWithCode';
 import BasicForm from '@/containers/clothes/BasicForm';
-import SearchWithImage from '@/containers/clothes/SearchWithImage';
 
 const Page = () => {
-  return (
-    <>
-      {/* <SearchWithImage /> */}
-      <BasicForm />
-      {/* <AdditionalForm /> */}
-      {/* <SearchWithCode /> */}
-    </>
-  );
+  return <BasicForm />;
 };
 
 export default Page;
