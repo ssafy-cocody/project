627e83f0f9436389b3eb167000c580d56aa050c4
박지영 <jiyounge628@gmail.com>
2024-03-21 10:47:26 +0900

    [FE] [S10P22A307-175] design: 전체적인 디자인 수정

--- frontend/src/components/SaveButton/index.tsx
+++ frontend/src/components/SaveButton/index.tsx
@@ -1,6 +1,6 @@
 import styles from './SaveButton.module.scss';
 
-// TODO: 버튼 클릭시 바텀시트 올라오는 핸들러 필요
+// TODO: 저장 핸들러
 const SaveButton = () => {
   return (
     <button className={styles['save-button']} type="button">
