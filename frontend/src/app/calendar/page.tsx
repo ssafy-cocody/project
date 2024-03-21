51ab71fc0022c481b057f3d705ed1c68fa9ac690
박지영 <jiyounge628@gmail.com>
2024-03-21 14:43:50 +0900

    [S10P22A307-228] feat: 캘린더 navigation scroll 훅 추가

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -1,17 +1,39 @@
+'use client';
+
+import { useRef, useState } from 'react';
+
 import Background from '@/components/background';
 import Header from '@/components/Header';
 import Nav from '@/components/Nav';
 import Calendar from '@/containers/calendar/Calendar';
+import styles from '@/containers/calendar/Calendar.module.scss';
+import useScrollDirection from '@/hooks/useScrollDirection';
 
 const Page = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [isScrolledToTop, setScrolled] = useState<boolean>(false);
+  const [isNavShow, setNavShow] = useState<boolean>(true);
+
+  useScrollDirection(
+    scrollRef,
+    () => {
+      setScrolled(false);
+      setTimeout(() => setNavShow(false), 500);
+    },
+    () => {
+      setNavShow(true);
+      setScrolled(true);
+    },
+  );
+
   return (
     <>
       <Background $backgroundColor="skyBlue" />
       <Header title="캘린더" />
-      <main>
+      <main className={styles.calendar}>
         <Calendar />
       </main>
-      <Nav />
+      {isNavShow && <Nav className={`${isScrolledToTop ? styles.scrollToTop : styles.scrollToBottom}`} />}
     </>
   );
 };
