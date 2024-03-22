902278cabccb5051c7bfb831577e168ac88eda54
MaSeongJin <making1104@naver.com>
2024-03-22 16:12:47 +0900

    [S10P22A307-234]chore:oauth 수정

--- backend/src/main/java/com/cocodi/security/presentation/controller/PublicController.java
+++ backend/src/main/java/com/cocodi/security/presentation/controller/PublicController.java
@@ -5,6 +5,7 @@ import com.cocodi.security.application.service.NonAuthService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/public")
 @RequiredArgsConstructor
@@ -26,8 +28,9 @@ public class PublicController {
             for (Cookie cookie : cookies) {
                 if ("refreshToken".equals(cookie.getName())) {
                     String refreshToken = cookie.getValue();
+                    log.info("refreshToken={}" ,refreshToken);
                     if(jwtTokenProvider.validateToken(refreshToken)) {
-                        String accessToken = nonAuthService.getAccessToken(cookie.getValue());
+                        String accessToken = nonAuthService.getAccessToken(refreshToken);
                         HttpHeaders headers = new HttpHeaders();
                         headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
                         String nickname = nonAuthService.getNickname(accessToken);
