b9cd742de6bcd1db30b414c10a3d1698a70dd20e
MaSeongJin <making1104@naver.com>
2024-03-22 13:11:26 +0900

    [S10P22A307-234]chore:oauth 수정

--- backend/src/main/java/com/cocodi/security/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/com/cocodi/security/infrastructure/config/SecurityConfig.java
@@ -44,8 +44,10 @@ public class SecurityConfig {
                 })
                 .authorizeHttpRequests(authorizeRequests -> {
                     authorizeRequests
-                            .requestMatchers("*").permitAll()
-                            .requestMatchers("api/auth").authenticated();
+                            .requestMatchers("/public/**").permitAll()
+                            .requestMatchers("/swagger-ui/**").permitAll()
+                            .requestMatchers("/v3/**").permitAll()
+                            .requestMatchers("/auth/**").authenticated();
                 })
                 .oauth2Login(oauth2Login -> {
                     oauth2Login
