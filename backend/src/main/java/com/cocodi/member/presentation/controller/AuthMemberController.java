2f860341e45d29678f46ca33f252e42404d8b527
MaSeongJin <making1104@naver.com>
2024-03-25 17:00:00 +0900

    [S19P22A307-234]chore:회원정보 수정 api 수정

--- backend/src/main/java/com/cocodi/member/presentation/controller/AuthMemberController.java
+++ backend/src/main/java/com/cocodi/member/presentation/controller/AuthMemberController.java
@@ -6,6 +6,7 @@ import com.cocodi.security.domain.model.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
