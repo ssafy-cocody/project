0b061af7274eef30ad03074f6c7381a9b462d304
박지영 <jiyounge628@gmail.com>
2024-03-25 10:54:33 +0900

    [S10P22A307-228] feat: calendar navigation 고정

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -1,39 +1,19 @@
 'use client';
 
-import { useRef, useState } from 'react';
-
 import Background from '@/components/Background';
 import Header from '@/components/Header';
 import Nav from '@/components/Nav';
 import Calendar from '@/containers/calendar/Calendar';
-import styles from '@/containers/calendar/Calendar.module.scss';
-import useScrollDirection from '@/hooks/useScrollDirection';
 
 const Page = () => {
-  const scrollRef = useRef<HTMLDivElement>(null);
-  const [isScrolledToTop, setScrolled] = useState<boolean>(false);
-  const [isNavShow, setNavShow] = useState<boolean>(true);
-
-  useScrollDirection({
-    ref: scrollRef,
-    downFunc: () => {
-      setScrolled(false);
-      setTimeout(() => setNavShow(false), 500);
-    },
-    upFunc: () => {
-      setNavShow(true);
-      setScrolled(true);
-    },
-  });
-
   return (
     <>
       <Background $backgroundColor="skyBlue" />
       <Header title="캘린더" />
-      <main className={styles.calendar}>
+      <main>
         <Calendar />
       </main>
-      {isNavShow && <Nav className={`${isScrolledToTop ? styles.scrollToTop : styles.scrollToBottom}`} />}
+      <Nav />
     </>
   );
 };
