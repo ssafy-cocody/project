5b7a9d623932abbe911796c30bc29e1635425031
박지영 <jiyounge628@gmail.com>
2024-03-25 12:21:17 +0900

    [S10P22A307-232] chore: 사용안하는 store 파일 삭제

--- frontend/src/containers/home/store.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'jotai';
-
-import { IRecommendCody } from '@/containers/home/type';
-
-const initRecommend: IRecommendCody = { id: 0, image: '' };
-const selectedCodyAtom = atom<IRecommendCody>(initRecommend);
-
-export { selectedCodyAtom };
