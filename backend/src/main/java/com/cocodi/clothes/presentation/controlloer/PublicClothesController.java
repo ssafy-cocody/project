9898681633d65f2c11a6a2db584ca7bb303843d9
SSAFY <jmg97@kakao.com>
2024-03-26 10:46:22 +0900

    [S10P22A307-266] feat : 이미지 업로드 기능 추가

--- backend/src/main/java/com/cocodi/clothes/presentation/controlloer/PublicClothesController.java
+++ backend/src/main/java/com/cocodi/clothes/presentation/controlloer/PublicClothesController.java
@@ -1,14 +1,15 @@
 package com.cocodi.clothes.presentation.controlloer;
 
 import com.cocodi.clothes.application.service.ClothesService;
+import com.cocodi.clothes.domain.model.Clothes;
+import com.cocodi.clothes.presentation.request.ClothesCreateRequest;
+import com.cocodi.clothes.presentation.response.ClothesResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicClothesController {
 
     private final ClothesService clothesService;
+    private final ObjectMapper objectMapper;
+
     @GetMapping("/temp/{uuid}")
     public ResponseEntity<byte[]> getTempImg(@PathVariable String uuid) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.IMAGE_PNG);
         return ResponseEntity.ok().headers(headers).body(clothesService.getTempImg(uuid));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createImg(@ModelAttribute ClothesCreateRequest clothesCreateRequest) {
+        Clothes clothes = clothesService.createClothes(clothesCreateRequest);
+        ClothesResponse clothesResponse = objectMapper.convertValue(clothes, ClothesResponse.class);
+        return ResponseEntity.ok(clothesResponse);
     }
 }
