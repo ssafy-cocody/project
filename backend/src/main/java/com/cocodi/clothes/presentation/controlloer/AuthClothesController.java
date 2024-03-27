6b61de08bf0d6624e36e0c2f65f6d0090390d11a
SSAFY <jmg97@kakao.com>
2024-03-27 15:04:32 +0900

    [S10P22A307-269] feat : 의상 업로드 완료

--- backend/src/main/java/com/cocodi/clothes/presentation/controlloer/AuthClothesController.java
+++ backend/src/main/java/com/cocodi/clothes/presentation/controlloer/AuthClothesController.java
@@ -98,4 +98,10 @@ public class AuthClothesController {
                 .map(clothes -> objectMapper.convertValue(clothes, ClothesResponse.class)).toList();
         return ResponseEntity.ok(clothesResponseList);
     }
+
+    @PostMapping("/temp/save/{uuid}")
+    public ResponseEntity<ClothesResponse> createTempInfo(@PathVariable String uuid, @ModelAttribute ClothesCreateRequest clothesCreateRequest) {
+        clothesService.createClothesTemp(uuid, clothesCreateRequest);
+        return null;
+    }
 }
