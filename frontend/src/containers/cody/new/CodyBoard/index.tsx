248d25215743149261a90336300c92ebec0269c7
박지영 <jiyounge628@gmail.com>
2024-03-27 14:24:36 +0900

    [S10P22A307-281] feat: 같은 카테고리 중복 클릭 시 토스트메세지 출력

--- frontend/src/containers/cody/new/CodyBoard/index.tsx
+++ frontend/src/containers/cody/new/CodyBoard/index.tsx
@@ -6,7 +6,7 @@ import { Dispatch, SetStateAction } from 'react';
 import TextInputWithUnderLine from '@/components/TextInputWithUnderLine';
 import styles from '@/containers/cody/new/CodyBoard/Board.module.scss';
 import { ISelectedClothes } from '@/containers/cody/new/type';
-import { IClothes } from '@/types/clothes';
+import { Category, IClothes, TCategory } from '@/types/clothes';
 
 interface Props {
   onClickDeleteClothes: () => void;
@@ -19,11 +19,20 @@ const CodyBoard = ({ onClickDeleteClothes, selectedClothes, setDeleteClothes }: 
     onClickDeleteClothes();
     setDeleteClothes(selectedClothes[category]);
   };
+
+  const categoryOrder: (keyof TCategory)[] = [Category.TOP, Category.OUTER, Category.BOTTOM, Category.SHOES];
+  const sortedSelectedClothes = categoryOrder.reduce((sortedClothes: ISelectedClothes, category) => {
+    if (selectedClothes[category]) {
+      sortedClothes[category] = selectedClothes[category];
+    }
+    return sortedClothes;
+  }, {});
+
   return (
     <div className={styles['board-container']}>
       <TextInputWithUnderLine label="새로운 코디명" />
       <div className={styles.board}>
-        {Object.keys(selectedClothes).map((category) => {
+        {Object.keys(sortedSelectedClothes).map((category) => {
           return (
             <button
               key={category}
