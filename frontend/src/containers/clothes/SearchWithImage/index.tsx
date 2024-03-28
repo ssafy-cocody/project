af8a3fe38edb4084b83835d1263d4a378142a102
hamster-wave <hamsterwave0925@gmail.com>
2024-03-28 15:07:54 +0900

    [S10P22A307-257] feat: 이미지 검색 api 연동

--- frontend/src/containers/clothes/SearchWithImage/index.tsx
+++ frontend/src/containers/clothes/SearchWithImage/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 import Button from '@/components/Button';
@@ -8,29 +9,41 @@ import SearchResult from '@/containers/clothes/SearchResult';
 import style from '@/containers/clothes/SearchWithImage/SearchWithImage.module.scss';
 import useClothesStep from '@/hooks/useClothesStep';
 import useModal from '@/hooks/useModal';
-import { fetchPostClothesImage } from '@/services/clothes';
+import { fetchGetClothesInfo, fetchPostClothesImage } from '@/services/clothes';
 import { Step } from '@/types/clothes';
 
 const SearchWithImage = ({ onClickButton }: { onClickButton: () => void }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { Modal, openModal } = useModal();
   const { jumpStep } = useClothesStep();
   const [multipartFile, setMultipartFile] = useState<File>();
+  const [clothesUuid, setClothesUuid] = useState('');
+  const [isFormValid, setIsFormValid] = useState(false);
 
-  const isValid = true;
-  const isLoading = false;
-  const isSuccess = false;
+  const clothesSearchItemQuery = useQuery({
+    queryKey: [''], // 캐싱할 필요가 없음
+    queryFn: () => fetchGetClothesInfo({ uuid: clothesUuid }),
+    enabled: !!clothesUuid,
+  });
+
+  const postClothesImage = useMutation({
+    mutationFn: fetchPostClothesImage,
+    onSuccess: (data) => {
+      setClothesUuid(data.uuid);
+    },
+  });
+
+  const isSuccess = clothesSearchItemQuery.isSuccess && !!clothesSearchItemQuery.data.length;
+  const isSearching = postClothesImage.isPending || clothesSearchItemQuery.isPending; // 유사한 옷 검색 중
 
   const modalTitle = () => {
-    if (isLoading) return '검색 중 입니다.';
-    if (!isSuccess) return '옷을 찾는데에 실패했어요. 😥';
-    return '옷을 선택해 주세요.';
+    if (isSearching) return '검색 중 입니다.';
+    if (isSuccess) return '옷을 선택해 주세요.';
+    return '옷을 찾는데에 실패했어요. 😥';
   };
 
   const modalContent = () => {
-    if (isLoading) return '';
-    if (isSuccess) return <SearchResult onClick={onClickButton} />;
-
+    if (isSearching) return '';
+    if (isSuccess) return <SearchResult onClick={onClickButton} clothesList={clothesSearchItemQuery.data} />;
     return (
       <>
         <Button onClick={() => jumpStep(Step.SEARCH_WITH_CODE)}>품번으로 검색하기</Button>
@@ -42,22 +55,18 @@ const SearchWithImage = ({ onClickButton }: { onClickButton: () => void }) => {
   };
 
   const handleImageChange = (file: File) => {
+    setIsFormValid((prev) => !prev);
     setMultipartFile(file);
   };
 
   const handleSearch = async () => {
-    if (!multipartFile) return;
+    if (!multipartFile || postClothesImage.isPending) return;
 
     const formData = new FormData();
     formData.append('multipartFile', multipartFile);
 
-    try {
-      const result = await fetchPostClothesImage({ formData });
-      console.log(result);
-    } catch (e) {
-      // TODO 실패시 모달 오픈
-      // openModal();
-    }
+    postClothesImage.mutate({ formData });
+    openModal();
   };
 
   return (
@@ -67,7 +76,7 @@ const SearchWithImage = ({ onClickButton }: { onClickButton: () => void }) => {
         <div className={style['tip-wrapper']}>
           <p className={style.tip}>💡 TIP. 옷을 가지런히 찍을수록 정확도가 높아집니다.</p>
         </div>
-        <Button type="button" disabled={!isValid} onClick={handleSearch}>
+        <Button type="button" disabled={!isFormValid} onClick={handleSearch}>
           검색
         </Button>
       </form>
