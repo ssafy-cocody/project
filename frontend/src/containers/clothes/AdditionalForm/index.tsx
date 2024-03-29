438e407ce79ba9c4c2548fe98e298a0db3cd1a66
hamster-wave <hamsterwave0925@gmail.com>
2024-03-29 15:07:56 +0900

    [S10P22A307-224] feat: price 인풋 숫자만

--- frontend/src/containers/clothes/AdditionalForm/index.tsx
+++ frontend/src/containers/clothes/AdditionalForm/index.tsx
@@ -1,25 +1,8 @@
+import { useState } from 'react';
+
 import Button from '@/components/Button';
 import TextInput from '@/components/TextInput';
 import styles from '@/containers/clothes/AdditionalForm/AdditionalForm.module.scss';
-
-const additionalFormInput = [
-  {
-    label: '브랜드',
-    key: 'brand',
-  },
-  {
-    label: '품번',
-    key: 'productNo',
-  },
-  {
-    label: '가격',
-    key: 'price',
-  },
-  {
-    label: '쇼핑몰 링크',
-    key: 'link',
-  },
-];
 
 const AdditionalForm = ({
   onClickButton,
@@ -28,19 +11,54 @@ const AdditionalForm = ({
   onClickButton: () => void;
   onChange: ({ key, value }: { key: string; value: string }) => void;
 }) => {
+  const [price, setPrice] = useState('');
+
+  const additionalFormInput = [
+    {
+      label: '브랜드',
+      key: 'brand',
+    },
+    {
+      label: '품번',
+      key: 'productNo',
+    },
+    {
+      label: '가격',
+      key: 'price',
+      value: price,
+    },
+    {
+      label: '쇼핑몰 링크',
+      key: 'link',
+    },
+  ];
+
   return (
     <div className={styles['form-container']}>
       <form action="">
-        {additionalFormInput.map(({ label, key }) => (
+        {additionalFormInput.map(({ label, key, value }) => (
           <TextInput
             key={key}
             label={label}
-            onChange={(e) =>
+            value={value}
+            onChange={(e) => {
+              // price는 숫자만 입력 가능
+              if (key === 'price') {
+                // eslint-disable-next-line no-underscore-dangle
+                const _price = Number(e.target.value);
+                if (e.target.value.length === 0) {
+                  setPrice('');
+                  return;
+                }
+                if (Number.isNaN(_price)) return;
+
+                setPrice(e.target.value);
+              }
               handleChange({
                 key,
                 value: e.target.value,
-              })
-            }
+              });
+            }}
           />
         ))}
         <div className={styles['button-wrapper']}>
