10bcc5ca105a2e44eda9004f86cbfdecce606288
박지영 <jiyounge628@gmail.com>
2024-03-29 18:20:32 +0900

    [S10P22A307-277] chore: react query devtools 의존성 추가

--- frontend/src/utils/Provider.tsx
+++ frontend/src/utils/Provider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import SessionProvider from '@/utils/SessionProvider';
 
@@ -10,6 +11,7 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider>{children}</SessionProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
     </QueryClientProvider>
   );
 };
