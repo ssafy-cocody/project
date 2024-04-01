d972b0704add6f73e93c30480f70a2bb725c5227
박지영 <jiyounge628@gmail.com>
2024-04-01 09:50:16 +0900

    [S10P22A307-277] feat: 카테고리 필터링 추가

--- frontend/src/components/ClothesList/index.tsx
+++ frontend/src/components/ClothesList/index.tsx
@@ -3,28 +3,30 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 
 import styles from '@/components/ClothesList/ClothesList.module.scss';
-import { useIntersectionObserver } from '@/hooks/useIntersectionObserver';
+import { useInfinityScroll } from '@/hooks/useInfinityScroll';
 import { fetchGetClothes } from '@/services/closet';
 import { IFetchGetClosetResponse } from '@/services/closet/type';
-import { IClothes } from '@/types/clothes';
+import { ClothesCategory, IClothes } from '@/types/clothes';
 
 interface Props {
   handleModal?: () => void;
   className?: string;
   onSelectClothes?: (newlyClickedClothes: IClothes) => void;
+  currentCategory?: keyof typeof ClothesCategory;
 }
 
 const PAGE_SIZE = 9;
 
-const ClothesList = ({ handleModal, className, onSelectClothes }: Props) => {
+const ClothesList = ({ handleModal, className, onSelectClothes, currentCategory }: Props) => {
   const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<IFetchGetClosetResponse>({
     queryKey: ['ClothesQueryKey'],
-    queryFn: ({ pageParam }) => fetchGetClothes({ page: pageParam as number, size: PAGE_SIZE }),
+    queryFn: ({ pageParam }) =>
+      fetchGetClothes({ page: pageParam as number, size: PAGE_SIZE, category: currentCategory }),
     initialPageParam: 0,
     getNextPageParam: (lastPage) => (lastPage.last ? undefined : lastPage.pageable.pageNumber + 1),
   });
 
-  const refLast = useIntersectionObserver({ hasNextPage, fetchNextPage }).ref;
+  const refLast = useInfinityScroll({ hasNextPage, fetchNextPage }).ref;
 
   return (
     <div className={`${styles['list-container']} ${className}`}>
@@ -32,20 +34,23 @@ const ClothesList = ({ handleModal, className, onSelectClothes }: Props) => {
         <div className={styles['clothes-container']}>
           {data?.pages.map(({ content }: { content: IClothes[] }) => {
             return content.map((item: IClothes) => {
-              const { clothesId, image } = item;
-              return (
-                <button
-                  type="button"
-                  className={styles['clothes-image-container']}
-                  key={clothesId.toString()}
-                  onClick={() => {
-                    handleModal ? handleModal() : '';
-                    onSelectClothes ? onSelectClothes(item) : '';
-                  }}
-                >
-                  <Image src={image!} alt={clothesId.toString()} fill className={styles.clothes} />
-                </button>
-              );
+              const { clothesId, image, category } = item;
+              if (!currentCategory || currentCategory === category) {
+                return (
+                  <button
+                    type="button"
+                    className={styles['clothes-image-container']}
+                    key={clothesId.toString()}
+                    onClick={() => {
+                      handleModal ? handleModal() : '';
+                      onSelectClothes ? onSelectClothes(item) : '';
+                    }}
+                  >
+                    <Image src={image!} alt={clothesId.toString()} fill className={styles.clothes} />
+                  </button>
+                );
+              }
+              return '';
             });
           })}
           {hasNextPage ? (
--- frontend/src/services/closet/index.ts
+++ frontend/src/services/closet/index.ts
@@ -5,11 +5,10 @@ import { IFetchGetClosetRequest, IFetchGetClosetResponse } from './type';
 export const fetchGetClothes = async ({
   page,
   size,
-  sort = 'string',
   category,
 }: IFetchGetClosetRequest): Promise<IFetchGetClosetResponse> => {
   const data: IFetchGetClosetResponse = await api.get<IFetchGetClosetResponse>(
-    `/closet?page=${page}&size=${size}&sort=${sort}&category=${category || ''}`,
+    `/closet?page=${page}&size=${size}&category=${category || ''}`,
   );
   return data;
 };
