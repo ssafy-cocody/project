1069aae42652892704e2e1790b19d921b37d63bc
MaSeongJin <making1104@naver.com>
2024-04-01 17:48:28 +0900

    [S19P22A307-234]chore:response 수정

--- backend/src/main/java/com/cocodi/codi/presentation/response/ImageSearchResponse.java
+++ backend/src/main/java/com/cocodi/codi/presentation/response/ImageSearchResponse.java
@@ -1,11 +1,13 @@
 package com.cocodi.codi.presentation.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record ImageSearchResponse(
-        List<ImageResponse> topList,
-        List<ImageResponse> bottomList,
-        List<ImageResponse> outerList,
-        List<ImageResponse> shoesList,
-        List<ImageResponse> onepieceList) {
+        @JsonProperty("TOP") List<ImageResponse> topList,
+        @JsonProperty("BOTTOM") List<ImageResponse> bottomList,
+        @JsonProperty("OUTER") List<ImageResponse> outerList,
+        @JsonProperty("SHOES") List<ImageResponse> shoesList,
+        @JsonProperty("ONEPIECE") List<ImageResponse> onepieceList) {
 }
