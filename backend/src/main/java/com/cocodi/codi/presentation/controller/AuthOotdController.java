7efbb1aaf6d7b9942cb09c0bfd7313ef36200a7f
MaSeongJin <making1104@naver.com>
2024-04-01 09:59:38 +0900

    [S19P22A307-234]chore:todo 완료 처리

--- backend/src/main/java/com/cocodi/codi/presentation/controller/AuthOotdController.java
+++ backend/src/main/java/com/cocodi/codi/presentation/controller/AuthOotdController.java
@@ -1,6 +1,7 @@
 package com.cocodi.codi.presentation.controller;
 
 
+import com.cocodi.aws.application.service.S3Service;
 import com.cocodi.clothes.application.service.ClosetService;
 import com.cocodi.codi.application.service.OotdService;
 import com.cocodi.codi.presentation.request.OotdCodyRequest;
@@ -26,6 +27,7 @@ public class AuthOotdController {
     private final OotdService ootdService;
     private final ClosetService closetService;
     private final SseService sseService;
+    private final S3Service s3Service;
 
     /**
      * 저장된 Ootd 조회 (한 달)
@@ -59,12 +61,12 @@ public class AuthOotdController {
      */
     @PostMapping("/image")
     public ResponseEntity<?> createOotdByImage(@RequestBody OotdImageRequest ootdCreateRequest, MultipartFile ootdImage, @AuthenticationPrincipal PrincipalDetails principalDetails) {
-        // TODO : s3 image upload
-        ootdService.createOotdByImage(ootdCreateRequest, "ootdImage", principalDetails.getMemberId());
-        // TODO : 날짜 조회 해서 등록 or 수정
-        // TODO : Image에서 옷 정보 뽑아서 코디 검색
-        //          -> 코디 있다면 해당 코디 가져와서 저장
-        //          -> 코디 없다면 새로운 코디 생성해서 저장
+        String imageUrl = s3Service.uploadDefault(ootdImage);
+        ootdService.createOotdByImage(ootdCreateRequest, imageUrl, principalDetails.getMemberId());
+        // 날짜 조회 해서 등록 or 수정
+        // Image에서 옷 정보 뽑아서 코디 검색
+        //   -> 코디 있다면 해당 코디 가져와서 저장
+        //   -> 코디 없다면 새로운 코디 생성해서 저장
         return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
 
