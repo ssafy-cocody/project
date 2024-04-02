6d8fa5ce2113d9e172456751db477178d794574f
박지영 <jiyounge628@gmail.com>
2024-04-02 18:37:33 +0900

    [S10P22A307-315] fix: 저장 버튼 children option으로 수정

--- frontend/src/app/mypage/profile/page.tsx
+++ frontend/src/app/mypage/profile/page.tsx
@@ -7,7 +7,7 @@ import styles from '@/containers/mypage/profile/ProfileLayout.module.scss';
 const Page = () => {
   return (
     <>
-      <Header previousLink="/mypage" title="내 정보 수정" RightComponent={<SaveButton />} />
+      <Header previousLink="/mypage" title="내 정보 수정" RightComponent={<SaveButton>저장</SaveButton>} />
       <div className={styles.container}>
         <ProfileCard />
       </div>
--- frontend/src/components/SaveButton/index.tsx
+++ frontend/src/components/SaveButton/index.tsx
@@ -3,7 +3,7 @@ import { ButtonHTMLAttributes } from 'react';
 import styles from './SaveButton.module.scss';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 const SaveButton = ({ children, ...props }: Props) => {
