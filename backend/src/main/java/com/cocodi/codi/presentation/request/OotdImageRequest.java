1eec46d7f961911606674826ac3a144485470e2b
MaSeongJin <making1104@naver.com>
2024-04-02 14:29:13 +0900

    [S19P22A307-234]chore:파이썬 연동 수정

--- backend/src/main/java/com/cocodi/codi/presentation/request/OotdImageRequest.java
+++ backend/src/main/java/com/cocodi/codi/presentation/request/OotdImageRequest.java
@@ -1,11 +1,12 @@
 package com.cocodi.codi.presentation.request;
 
-import com.cocodi.clothes.presentation.request.ClothesRequest;
+import com.cocodi.clothes.presentation.request.ClothesPythonRequest;
 
 import java.time.LocalDate;
 
 public record OotdImageRequest(
-        ClothesRequest clothesRequest,
+
+        ClothesPythonRequest clothesRequest,
         LocalDate date
 ) {
 }
---
+++ backend/src/main/java/com/cocodi/codi/presentation/response/RecommendItemResponse.java
@@ -0,0 +1,7 @@
+package com.cocodi.codi.presentation.response;
+
+public record RecommendItemResponse() {
+
+
+
+}
