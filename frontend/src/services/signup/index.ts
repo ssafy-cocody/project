128bc4ce34ae2b89202f1d06bb3665e40f61c6db
박진아 <dndbekfrl1@gmail.com>
2024-04-02 19:07:20 +0900

    [FE] [S10P22A307-255] 회원 정보 수정 + 회원가입 수정

--- frontend/src/services/signup/index.ts
+++ frontend/src/services/signup/index.ts
@@ -1,15 +1,49 @@
-import { api } from '@/services';
-import { IFetchCreateMemberRequest } from '@/services/signup/type';
+import { BASE_URL, getAccessToken } from '@/services';
+import { IFetchCreateMemberRequest, IFetchUpdateMemberRequest } from '@/services/signup/type';
 
 /**
  * 회원가입
+ * @param birth string,
+ * @param gender, TGender
+ * @param nickname, string
  */
-const fetchCreateMember = async (params: IFetchCreateMemberRequest) => {
-  const response = await api.patch<IFetchCreateMemberRequest>('/member', {
-    ...params,
+async function fetchCreateMember({ formData }: IFetchCreateMemberRequest) {
+  const response = await fetch(`${BASE_URL}/auth/v1/member`, {
+    method: 'PATCH',
+    body: formData,
+    headers: {
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
   });
 
-  return response;
+  if (!response.ok) return new Error(response.statusText);
+
+  const data = await response.json();
+
+  return data;
+}
+
+/**
+ * 회원 정보 수정
+ * @param birth string,
+ * @param gender, TGender
+ * @param nickname, string
+ * @param profile, string
+ */
+const fetchUpdateMember = async ({ formData }: IFetchUpdateMemberRequest) => {
+  const response = await fetch(`${BASE_URL}/auth/v1/member`, {
+    method: 'PATCH',
+    body: formData,
+    headers: {
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  });
+
+  if (!response.ok) return new Error(response.statusText);
+
+  const data = await response.json();
+
+  return data;
 };
 
-export { fetchCreateMember };
+export { fetchCreateMember, fetchUpdateMember };
--- frontend/src/services/signup/type.ts
+++ frontend/src/services/signup/type.ts
@@ -1,5 +1,9 @@
-import { IUser } from '@/types/user';
+type IFetchCreateMemberRequest = {
+  formData: FormData; // birth, gender, nickname
+};
 
-type IFetchCreateMemberRequest = Pick<IUser, 'birth' | 'gender' | 'nickname'>;
+interface IFetchUpdateMemberRequest {
+  formData: FormData; // birth, gender, nickname, profile
+}
 
-export type { IFetchCreateMemberRequest };
+export type { IFetchCreateMemberRequest, IFetchUpdateMemberRequest };
