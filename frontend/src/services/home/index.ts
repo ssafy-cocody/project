8000444ea38f03770498ffd8ebdd8f3038d7f11a
hamster-wave <hamsterwave0925@gmail.com>
2024-04-03 17:49:41 +0900

    [S10P22A307-358] fix: 내가 만든 코디 8 -> 6개

--- frontend/src/services/home/index.ts
+++ frontend/src/services/home/index.ts
@@ -3,9 +3,10 @@ import { IFetchGetMyCodyResponse } from '@/services/home/type';
 
 /**
  * 내가 만든 코디
- * @returns 최대 코디 8개 반환
+ * @returns 최대 코디 6개 반환
  */
 export const fetchGetMyCody = async () => {
-  const data = await api.get<IFetchGetMyCodyResponse>('/cody');
+  const size = 6;
+  const data = await api.get<IFetchGetMyCodyResponse>(`/cody?size=${size}`);
   return data;
 };
