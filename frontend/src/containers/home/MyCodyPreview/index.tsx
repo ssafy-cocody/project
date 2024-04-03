3f9971bde26dc0d9176853e0d9d988668cea0030
hamster-wave <hamsterwave0925@gmail.com>
2024-04-03 18:15:04 +0900

    [S10P22A307-358] feat: 내가 만든 코디 api 연동

--- frontend/src/containers/home/MyCodyPreview/index.tsx
+++ frontend/src/containers/home/MyCodyPreview/index.tsx
@@ -1,34 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
 
 import Button from '@/components/Button';
 import styles from '@/containers/home/MyCodyPreview/Preview.module.scss';
+import { fetchGetMyCody } from '@/services/home';
 
 const CODY_COUNT = 6;
 
+const MY_CODY_QUERY_KEY = ['homeMyCodyPreview'];
+
 const MyCodyPreview = () => {
-  const [codies] = useState([
-    { id: 1, image: '/images/test1.jpg' },
-    { id: 2, image: '/images/test2.jpg' },
-    { id: 3, image: '/images/test3.jpg' },
-    { id: 4, image: '/images/test4.jpg' },
-    { id: 5, image: '/images/test5.jpg' },
-  ]);
+  const { data, isLoading } = useQuery({ queryKey: MY_CODY_QUERY_KEY, queryFn: () => fetchGetMyCody() });
+
+  const codies = data?.content;
+  const loadingArray = Array.from({ length: CODY_COUNT }, (_, idx) => idx);
 
   return (
     <div className={styles.container}>
       <div className={styles.title}>내가 만든 코디</div>
       <div className={styles.showcase}>
         <div className={styles.codies}>
-          {codies.map(({ id, image }, index) => {
-            return (
-              <div key={id} className={styles['cody-image']}>
-                <Image src={image} alt={`나의 코디 ${index}`} fill />
-              </div>
-            );
-          })}
-          {codies.length < CODY_COUNT ? (
+          {/** 로딩 중일때 빈 박스 */}
+          {isLoading && loadingArray.map((idx) => <div key={idx} className={styles['cody-image']} />)}
+
+          {codies &&
+            codies.length > 0 &&
+            codies.map(({ codiId, image }, index) => {
+              return (
+                <div key={codiId} className={styles['cody-image']}>
+                  <Image src={image} alt={`나의 코디 ${index}`} fill />
+                </div>
+              );
+            })}
+          {codies && codies.length < CODY_COUNT ? (
             Array.from({ length: CODY_COUNT - codies.length }, (_, i: number) => i).map((value) => {
               return <div key={-value} className={styles['cody-image']} />;
             })
