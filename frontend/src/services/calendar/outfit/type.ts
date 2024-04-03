0aa781e95e2c745b5e2c8524970164aa80593fb6
hamster-wave <hamsterwave0925@gmail.com>
2024-04-03 16:58:44 +0900

    [S10P22A307-342] delete: 사용하지 않는 주석 제거

--- frontend/src/services/calendar/outfit/type.ts
+++ frontend/src/services/calendar/outfit/type.ts
@@ -1,21 +1,6 @@
 import { IClothes } from '@/types/clothes';
 
-/** 
-type TSimilarClothes = {
-  clothesId: number;
-  imageUrl: string;
-};
-*/
 type IFetchGetOotdImageResponse = Record<string, IClothes[]>;
-/** 
-{
-  // TODO TOP, BOTTOM, OUTER, SHOES 로 변경
-  topList: TSimilarClothes[];
-  bottomList: TSimilarClothes[];
-  outerList: TSimilarClothes[];
-  shoesList: TSimilarClothes[];
-}
-*/
 
 interface IFetchGetOotdImageRequest {
   formData: FormData;
